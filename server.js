7758f7bd084d4d50940ea90b30ecde61b27ee2b1
Ben Uthoff <81537940+benuthoff@users.noreply.github.com>
2024-06-25 13:29:29 +0000

    dev

--- server.js
+++ server.js
@@ -10,8 +10,23 @@ const server = require('http').createServer(app);
 
 const port = 4222;
 
-app.use(express.static('source'));
+
+/* Database Management */
+const sqlite3 = require('sqlite3');
+const db_file = '';
+
+const testdb = new sqlite3.Database(db_file, callback=()=>{
+    console.log(`Opened database /${db_file}`);
+});
+
+/* Server Endpoints */
+
+app.get('/', (req, res)=>{ // Home.
+    res.sendFile('/workspaces/blog/source/index.html');
+});
+
+app.use('/static', express.static('static')); // Static Files. (js/css)
 
 app.listen(port, () => {
-    console.log(`Listening on :${port}`)
+    console.log(`Listening on :${port}`);
 });
